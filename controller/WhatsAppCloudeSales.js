d8d11564c447111159e8f5f9180223caccf49fc5
Jishnu M <mjihnu990@gmail.com>
2025-02-11 15:02:38 +0530

    code

--- controller/WhatsAppCloudeSales.js
+++ controller/WhatsAppCloudeSales.js
@@ -7,7 +7,7 @@ const WHATSAPP_CLOUD_API_URL = "https://graph.facebook.com/v18.0/";
 const WHATSAPP_ACCESS_TOKEN = process.env.WHATSAPP_ACCESS_TOKEN;
 const WHATSAPP_PHONE_NUMBER_ID = process.env.WHATSAPP_PHONE_NUMBER_ID;
 
-import axios from "axios";
+
 
 const sendWhatsAppMessage = async (phone, message) => {
     try {
