410afd7781bfa3150396033b1ff0edbdba5b8d11
Jishnu M <mjihnu990@gmail.com>
2025-02-11 14:59:24 +0530

    test1

---
+++ controller/Test.js
@@ -0,0 +1,27 @@
+
+import bodyParser from 'body-parser'
+
+app.use(bodyParser.json());
+
+const VERIFY_TOKEN = "EAAIxXzYDBZAwBOZCZAncmShNjxNevPFMeiToZCnBmFxnD4HC1uzwIdCF8dYZByq1EofKeXQMfSgcdS6rTtwajw4ZCBEa0SevvLJNxldcZCHGqQbuFpaqpV0nTYrqz0T6ibWIakoUIMCYqTqEvcuZBHRJYIgV7Evio8Ckb6y39uA02GIwTuqhLC675RYc6p7llrmjJwZDZD";
+
+// Webhook verification
+export const GETWHATSAPP = async (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode && token === VERIFY_TOKEN) {
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+};
+
+// Handle incoming messages
+export const POSTWHATSAPP = async (req, res) => {
+  console.log("Webhook received:", JSON.stringify(req.body, null, 2));
+  res.sendStatus(200);
+};
+
+
--- route/Whatsapp.js
+++ route/Whatsapp.js
@@ -2,11 +2,13 @@ import express from 'express';
 // import { salesWhatsAppZoho, } from '../controller/WhatsappController.js'
 import { WhatsAppZoho } from '../controller/WhatsAppCloudApi.js'
 import { WhatsAppSalesZoho } from '../controller/WhatsAppCloudeSales.js';
+import { GETWHATSAPP, POSTWHATSAPP } from '../controller/Test.js';
 
 const router = express.Router();
 router.post('/invoice-webhook', WhatsAppZoho);
 router.post('/sales-webhook', WhatsAppSalesZoho);
-router.post('/WhatsappApiwebhook', WhatsAppSalesZoho);
+router.post('/WhatsappApiwebhook', POSTWHATSAPP);
+router.get('/WhatsappApiwebhook', GETWHATSAPP);
 
 
 export default router;
